95814d81e7d6bc65c71735fc3d865b83309584de
Nadav Elgrabli <erezelgrably@hotmail.com>
2024-02-03 15:55:42 +0200

    add components to app modules

--- Client/src/app/modules/app.module.ts
+++ Client/src/app/modules/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from '../app.component';
+import { HomePageComponent } from '../components/home-page/home-page.component';
+import { NavBarComponent } from '../components/nav-bar/nav-bar.component';
+import { SignUpComponent } from '../components/sign-up/sign-up.component';
+import { ChatBtnComponent } from '../components/chat-btn/chat-btn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent,
+    SignUpComponent,
+    ChatBtnComponent,
   ],
   imports: [
     BrowserModule,
