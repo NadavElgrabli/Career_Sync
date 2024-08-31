e31d495f06c25819e0ce87676a86f5e75b32d76a
Nadav Elgrabli <erezelgrably@hotmail.com>
2024-08-31 16:47:12 +0300

    chat fix

--- Client/src/app/components/chat/chat.component.ts
+++ Client/src/app/components/chat/chat.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SessionService } from 'src/app/services/sessionService';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent {
 
+export class ChatComponent implements OnInit {
+  constructor(private sessionService: SessionService) {}
+
+  ngOnInit(): void {}
+
+  isLoggedIn(): boolean {
+    return this.sessionService.getUserFromSession() !== null;
+  }
 }
