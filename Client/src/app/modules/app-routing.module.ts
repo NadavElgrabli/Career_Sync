6156ce8621d92bb14b14c55bbc89480de26228a6
Nadav Elgrabli <erezelgrably@hotmail.com>
2024-11-02 22:51:08 +0200

    add careers page

--- Client/src/app/modules/app-routing.module.ts
+++ Client/src/app/modules/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomePageComponent } from '../components/home-page/home-page.component';
 import { AboutComponent } from '../components/about/about.component';
 import { SignUpComponent } from '../components/sign-up/sign-up.component';
 import { ChatComponent } from '../components/chat/chat.component';
+import { CareersPageComponent } from '../components/careers-page/careers-page.component';
 
 const routes: Routes = [
   { path: 'home-page', component: HomePageComponent },
   { path: 'about', component: AboutComponent },
   { path: 'chat', component: ChatComponent },
   { path: 'sign-up-page', component: SignUpComponent },
+  { path: 'careers-page', component: CareersPageComponent },
   { path: '**', redirectTo: 'home-page' },
 ];
 
